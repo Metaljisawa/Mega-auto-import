fb87c8cd7060313041c72599c95e12033737c9c4
Metaljisawa <48813296+Metaljisawa@users.noreply.github.com>
2022-12-20 00:25:22 +0100

    Update mega.py

--- mega.py
+++ mega.py
@@ -5,12 +5,12 @@ mega_client = mega.MegaClient()
 mega_client.login('your@email.com', 'yourpassword')
 
 # Create a folder in Mega.nz
-folder_name = 'Recherche Master'
+folder_name = 'YourNameFolder'
 folder = mega_client.create_folder(folder_name)
 print(F'Folder has been created with Name "{folder_name}" and URL: "https://mega.nz/folder/{folder["h"]}".')
 
-# Iterate through all the files in the "Recherche Master" folder on your local storage
-local_folder = '/path/to/Recherche Master'
+# Iterate through all the files in the "Your name Folder" folder on your local storage
+local_folder = '/path/to/YourNameFolder'
 for root, dirs, files in os.walk(local_folder):
     for file in files:
         # Construct the file's path on your local machine
